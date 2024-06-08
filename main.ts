ba7127eca0075bb945481fb4ef76d2723bf96f2e
inagakih120418 <inagakih120418@gmail.com>
2024-06-08 19:14:34 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
 basic.forever(function () {
-	
+    if (robots.getSensorVar() > 500) {
+        robots.inputMoter(1023, 1023)
+    } else {
+        robots.inputMoter(700, -700)
+        basic.pause(150)
+        robots.stopMoter()
+        basic.pause(200)
+    }
 })
