df85664a194604e2334dfca170453579277444a8
ShuABE1616 <125678937+ShuABE1616@users.noreply.github.com>
2026-09-10 22:14:41 +0900

    makeButtonmode

--- main.ts
+++ main.ts
@@ -1,6 +1,3 @@
 basic.forever(function () {
-    IRCbitCar.whileRunning(function () {
-        // ここに好きなブロックを入れる（例：直進する）
-        IRCbitCar.inputMoter(1023, 1023)
-    })
+    IRCbitCar.inputMoter(1023, 1023)
 })
